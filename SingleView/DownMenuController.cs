440012205ae16f6e648c2e7b118c5243b9bfcdfc
DevelopmentApps <developmentapps@outlook.com>
2016-05-02 10:52:22 -0500

    modified the main.storyboard view controllers space to generic view instead of iPhone 4s

---
+++ SingleView/DownMenuController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace SingleView
+{
+	partial class DownMenuController : UIViewController
+	{
+		public DownMenuController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
